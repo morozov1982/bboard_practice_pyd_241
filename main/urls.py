15c179e056f98b8c622963bbef5becbddb7e47ac
morozov1982 <morozov1982@gmail.com>
2025-03-12 15:48:53 +0500

    lesson_53: start

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from main.views import index
+from main.views import index, other_page
 
 app_name = 'main'
 
 urlpatterns = [
+    path('<str:page>', other_page, name='other'),
     path('', index, name='index'),
 ]
