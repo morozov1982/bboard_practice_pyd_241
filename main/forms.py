f9d25f7dd47eed18dae28567c3ad98d12c0d900e
Alexandr Morozov <morozov1982@gmail.com>
2025-03-28 17:30:01 +0500

    lesson_59: Comment

--- main/forms.py
+++ main/forms.py
@@ -1,9 +1,10 @@
+from captcha.fields import CaptchaField
 from django import forms
 from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError
 from django.forms.models import inlineformset_factory
 
-from main.models import AdvUser, SuperRubric, SubRubric, Bb, AdditionalImage
+from main.models import AdvUser, SuperRubric, SubRubric, Bb, AdditionalImage, Comment
 from main.signals import post_register
 
 
@@ -86,3 +87,20 @@ class BbForm(forms.ModelForm):
 
 
 AIFormSet = inlineformset_factory(Bb, AdditionalImage, fields='__all__')
+
+
+class UserCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ('is_active',)
+        widgets = {'bb': forms.HiddenInput}
+
+
+class GuestCommentForm(forms.ModelForm):
+    captcha = CaptchaField(label='Введите текст с картинки',
+                           error_messages={'invalid': 'Неправильный текст'})
+
+    class Meta:
+        model = Comment
+        exclude = ('is_active',)
+        widgets = {'bb': forms.HiddenInput}
